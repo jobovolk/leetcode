09377b56840670545d5a45691ae7c3f8a1ae62f8
Josh Volkman <jobovolk@gmail.com>
2022-09-20 13:59:06 -0600

    adapted solution from comments

--- 417-pacific-atlantic-flow.py
+++ 417-pacific-atlantic-flow.py
@@ -1,44 +1,55 @@
 from typing import List
-from pandas import DataFrame
-class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        map = heights[:] 
-        # for row in map:
-        #     row.append('A')
-        #     row.insert(0, 'P')
-        # map.append(['A'] * len(row))
-        # map.insert(0, ['P'] * len(row))
-        return map
-    def checkEast(self, row):
-        # returns True if row is monotonically increasing
-        # add an index, (e.g. row[i:]), record the last index where true
-        return all(x<=y for x, y in zip(row, row[1:]))
+class Solution():
+    def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+        # check for an empty matrix
+        if not matrix:
+            return []
+        # initialize rows, cols as length of outer list, length of first inner list (must be a square matrix)
+        rows, cols = len(matrix), len(matrix[0])
+        pacific_reached = set()
+        atlantic_reached = set()
+        directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
-    def makeColumns(self, map):
-        frame = DataFrame(map).transpose()
+        def traverse(i, j, visited):
+            if (i, j) in visited:
+                return
+            visited.add((i, j))
+            # traverse neighbors
+            for dir in directions:
+                next_i, next_j = i + dir[0], j + dir[1]
+                if 0 <= next_i < rows and 0 <=next_j < cols:
+                    if matrix[next_i][next_j] >= matrix[i][j]:
+                        traverse(next_i, next_j, visited)
+        for row in range(rows):
+            traverse(row, 0, pacific_reached)
+            traverse(row, cols -1, atlantic_reached)
+        for col in range(cols):
+            traverse(0, col, pacific_reached)
+            traverse(rows-1, col, atlantic_reached)
+        return list(pacific_reached & atlantic_reached)
+
+
+
+
+
+
+
+    
+    #     # returns True if row is monotonically increasing
+    #     # add an index, (e.g. row[i:]), record the last index where true
+    #     return 
+
+    # def makeColumns(self, map):
+    #     frame = DataFrame(map).transpose()
         
-        # get columns from dataframe
-        return frame
+    #     # get columns from dataframe
+    #     return frame
 # could flip dataframe all four directions and run monotonicity check on each
 # monotonicity check has to return index up to which it is true 
+# all edge positions only have to check opposite direction of edge
                
-
 s = Solution()
-#%%
 heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
-for row in heights:
-    pairs = zip(row, row[1:])
-    print(dict(pairs))
-    
-#%%
+test = s.pacificAtlantic(heights)
+print(test)
 
-map = s.pacificAtlantic(heights)
-# print(s.pacificAtlantic(heights))
-testEast = s.checkEast(heights[0])
-testFrame = s.makeColumns(map)
-print(testEast)
-for row in map:
-    print(row[0])
-
-print(f"{testFrame=} ")
-# %%
